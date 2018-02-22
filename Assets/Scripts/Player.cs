8f7d74d96477e85b3d9794b27e154105e490a8a0
Pipas_ <19pipas@gmail.com>
2018-02-22 18:16:39 +0000

    Imported files and .gitignore

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int moveSpeed = 2;
+    public int turnRate = 1;
+
+    public int rotation;
+    private Vector2 direction;
+    private int angle = 0;
+    private bool isMoving;
+
+	// Use this for initialization
+	void Start ()
+    {
+        isMoving = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckKey();
+
+		if(isMoving)
+        {
+            transform.Translate(direction*moveSpeed*Time.deltaTime);
+            transform.Rotate(0, 0, turnRate*rotation*Time.deltaTime);
+        }
+	}
+
+    void CheckKey()
+    {
+        direction = Vector2.zero;
+        rotation = 0;
+
+        if (Input.GetKey(KeyCode.W))
+            direction += Vector2.up;
+        if (Input.GetKey(KeyCode.S))
+            direction += Vector2.down;
+        if (Input.GetKey(KeyCode.D))
+            rotation -= turnRate;
+        if (Input.GetKey(KeyCode.A))
+            rotation += turnRate;
+    }
+}
