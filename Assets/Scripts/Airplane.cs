b041e226170b98c3312716743b4e6177c3684559
Pipas_ <19pipas@gmail.com>
2018-03-07 19:19:40 +0000

    Removed Debug Logs

--- Assets/Scripts/Airplane.cs
+++ Assets/Scripts/Airplane.cs
@@ -25,8 +25,6 @@ public class Airplane : MonoBehaviour
                         ForceMode2D.Impulse);
 			}*/
 
-			Debug.Log(airplaneRB.velocity);
-
 			if(Mathf.Abs(airplaneRB.velocity.x) < 4 && Mathf.Abs(airplaneRB.velocity.y) < 4)
 			{
 				airplaneRB.AddForce(new Vector2(-airplaneRB.velocity.x, -airplaneRB.velocity.y)/3,
